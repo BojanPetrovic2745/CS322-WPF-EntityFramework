756194535d47daf512118f2568809a503cb3fdc0
bojan <bojan.petrovic.2745@outlook.com>
2020-11-17 21:33:39 +0100

    Add project files.

---
+++ CS322-WPF-EntityFramework-BojanPetrovic2745/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS322_WPF_EntityFramework_BojanPetrovic2745
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        wpfCrudEntities _db = new wpfCrudEntities();
+        public static DataGrid dataGrid;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+        }
+
+        private void Load()
+        {
+            myDataGrid.ItemsSource = _db.members.ToList();
+            dataGrid = myDataGrid;
+        }
+
+        private void insertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            InsertPage Ipage = new InsertPage();
+            Ipage.ShowDialog();
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+           int id = (myDataGrid.SelectedItem as member).Id;
+           UpdatePage Upage = new UpdatePage(id);
+           Upage.ShowDialog();
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int Id = (myDataGrid.SelectedItem as member).Id;
+            var deleteMember = _db.members.Where(m => m.Id == Id).Single();
+            _db.members.Remove(deleteMember);
+            _db.SaveChanges();
+            myDataGrid.ItemsSource = _db.members.ToList();
+        }
+    }
+}
---
+++ CS322-WPF-EntityFramework-BojanPetrovic2745/UpdatePage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CS322_WPF_EntityFramework_BojanPetrovic2745
+{
+    /// <summary>
+    /// Interaction logic for UpdatePage.xaml
+    /// </summary>
+    public partial class UpdatePage : Window
+    {
+        wpfCrudEntities _db = new wpfCrudEntities();
+        int Id;
+
+        public UpdatePage(int memberId)
+        {
+            InitializeComponent();
+            Id = memberId;
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            member updateMember = (from m in _db.members
+                                   where m.Id == Id
+                                   select m).Single();
+            updateMember.name = tbName.Text;
+            updateMember.gender = cbGender.Text;
+            _db.SaveChanges();
+            MainWindow.dataGrid.ItemsSource = _db.members.ToList();
+            this.Hide();
+        }
+    }
+}
